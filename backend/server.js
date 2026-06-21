16d174823a3d39de407195ad4d132c63f273077d
Aditya Singh <adityasinggh05@gmail.com>
2026-06-21 19:31:54 +0530

    Add user and message models, database connection, and update server configuration
    - Introduced user and message schemas for MongoDB.
    - Implemented database connection logic in a new db.js file.
    - Updated server.js to include middleware and health check endpoint.
    - Added @clerk/express dependency in package.json and package-lock.json.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import User from "./src/models/user.model.js";
+import { connectDb } from "./src/lib/db.js";
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
+const FROUNTEND_URL = process.env.FROUNTEND_URL;
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(express.json());
+app.use(cors({origin:FROUNTEND_URL, credentials:true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req,res)=> {
+    res.status(200).json({ok:true});
+});
+
+app.listen(port, () => {
+    connectDb();
+    console.log(`Server running on port ${port}`)
+});
 
 
 
---
+++ backend/src/lib/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+export const connectDb = async() => {
+    try{
+       const mongoUri = process.env.MONGO_URI;
+
+       if(!mongoUri){
+        throw new Error ("MONGO_URI is required");
+       }
+       const conn = await mongoose.connect(mongoUri);
+       console.log("MongoDB connected", conn.connection.host);
+    } catch(error){
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1); // 1 means failed, 0 means success
+    }
+}
